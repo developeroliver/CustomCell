6bc29abf2a2898a01fdeaa202614d10588e4ecb4
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-17 09:57:40 +0100

    project done

--- CustomTableViewCell/Cell/CustomCell.swift
+++ CustomTableViewCell/Cell/CustomCell.swift
@@ -8,16 +8,33 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+        ])
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func configure(withName name: String) {
+        nameLabel.text = name
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- CustomTableViewCell/Controller/ViewController.swift
+++ CustomTableViewCell/Controller/ViewController.swift
@@ -10,29 +10,41 @@ import UIKit
 class ViewController: UIViewController {
     
     let users       = ["codewitholiver", "alise.code", "dieyuna", "tarek_tgn", "battersya"]
-    var customTV: UITableView!
-    let reuseID     = "Users"
-
+    var tableView = UITableView()
+    let cellIdentifier = "CustomCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureSetup()
-        configureTableView()
-    }
-
-    
-    func configureSetup() {
+        
         view.backgroundColor = .systemBackground
         title = "Instagram Users"
         navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
-    
-    func configureTableView() {
-        customTV                = UITableView(frame: view.bounds)
-        customTV.dataSource     = self
-        customTV.delegate       = self
-        customTV.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        
+        tableView                = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CustomCell.self, forCellReuseIdentifier: cellIdentifier)
+        view.addSubview(tableView)
     }
 }
 
+
+extension ViewController:  UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return users.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CustomCell
+            let name = users[indexPath.row]
+            cell.accessoryType = .disclosureIndicator
+            cell.configure(withName: name)
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 60 // Hauteur de chaque cellule
+        }
+    
+    
+}
